0f18ad18444bc648451bf715ec8a43581849be88
zhenhua-xailient <zhenhua@xailient.com>
2020-04-15 14:46:05 +1000

    added inference time calculation

--- src/detector.c
+++ src/detector.c
@@ -970,8 +970,10 @@ float validate_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
         args.resized = &buf_resized[t];
         thr[t] = load_data_in_thread(args);
     }
+    int num_images = 0;
     time_t start = time(0);
     for (i = nthreads; i < m + nthreads; i += nthreads) {
+        num_images = i;
         fprintf(stderr, "\r%d", i);
         for (t = 0; t < nthreads && (i + t - nthreads) < m; ++t) {
             pthread_join(thr[t], 0);
@@ -1301,6 +1303,7 @@ float validate_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
     free(fp_for_thresh_per_class);
 
     fprintf(stderr, "Total Detection Time: %d Seconds\n", (int)(time(0) - start));
+    printf("images = %d, total_time = %d, inference_time = %d, fps = %\n", num_images, (int)(time(0) - start), ((float)(time(0) - start))/((float)num_images);
     printf("\nSet -points flag:\n");
     printf(" `-points 101` for MS COCO \n");
     printf(" `-points 11` for PascalVOC 2007 (uncomment `difficult` in voc.data) \n");
